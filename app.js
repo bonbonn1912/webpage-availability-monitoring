0f54c9e56521c731781c1e61066efffa7eb70559
Dominik Wolf <spam1@dominikw.de>
2021-03-05 17:48:26 +0100

    Adjust Pollrate

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var httpServer = http.createServer(app);
 var io = require("socket.io")(httpServer);
 
 const PORT = process.env.PORT || 8080;
-const PollRate = 6000;
+const PollRate = 180000;
 
 var GlobalTime;
 var LastTime;
