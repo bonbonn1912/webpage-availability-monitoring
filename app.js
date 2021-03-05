1dd76d503003723ec8073979f372bc20bf3a997b
Dominik Wolf <spam1@dominikw.de>
2021-03-05 13:25:14 +0100

    Websocket Verbindung hergestellt

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-//var http = require("http"); 
+var http = require("http"); 
 
 var express = require("express");
 var app = express();
+var httpServer = http.createServer(app);
+
+var io = require("socket.io")(httpServer);
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
@@ -10,5 +13,10 @@ app.get("/", function(req,res){
     res.render("index");
 });
 
-app.listen(3000);
+
+let PORT = 3000;
+httpServer.listen(PORT, function(){
+    console.log("Server listening on Port: "+PORT);
+});
+
 
